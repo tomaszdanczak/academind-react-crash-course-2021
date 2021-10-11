489fb312a489a868b1f57482bed4832ea9a3e531
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-11 16:17:06 +0200

    feat: add styles to the MainNavigation component

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
-        <li>
-          <Link to="/">All Meetups</Link>
-        </li>
-        <li>
-          <Link to="/new-meetup">Add New Meetup</Link>
-        </li>
-        <li>
-          <Link to="/favorites">My Favorites</Link>
-        </li>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">My Favorites</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
