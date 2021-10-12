1fa312504f4e2b503eaecabb49d1ea831f23e8df
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-12 17:42:40 +0200

    feat: add logic to the Context

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import FavoritesContextProvider from "./store/favorites-context";
+import { FavoritesContextProvider } from "./store/favorites-context";
 
 ReactDOM.render(
   <FavoritesContextProvider>
--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,9 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavoritesContext = createContext();
 
-export default function FavoritesContextProvider({ children }) {
+export function FavoritesContextProvider({ children }) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorite) => {
+      return prevUserFavorite.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorite) => {
+      return prevUserFavorite.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavorite: addFavoriteHandler,
+    removeFavorite: removeFavoriteHandler,
+    itemIsFavorite: itemIsFavoriteHandler,
+  };
+
   return (
-    <FavoritesContext.Provider value={{}}>{children}</FavoritesContext.Provider>
+    <FavoritesContext.Provider value={context}>
+      {children}
+    </FavoritesContext.Provider>
   );
 }
+
+export default FavoritesContext;
