639866e3a93f8f83e23495e50a249e22be7842e6
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-13 08:33:38 +0200

    feat: render components using data from Context

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,3 +1,22 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 export default function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const { favorites, totalFavorites } = useContext(FavoritesContext);
+
+  let content;
+
+  if (totalFavorites === 0) {
+    content = <p>You got no afvorites yet. Start adding some</p>;
+  } else {
+    content = <MeetupList meetups={favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>Favorites Page</h1>
+      {content}
+    </section>
+  );
 }
