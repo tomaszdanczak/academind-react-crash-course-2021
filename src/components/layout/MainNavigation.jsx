07879f839a07cd8ff68caa8c38924c661f4d8e89
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-13 08:59:43 +0200

    feat: render badge using data from Context

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation() {
+  const { totalFavorites } = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -10,11 +14,15 @@ export default function MainNavigation() {
           <li>
             <Link to="/">All Meetups</Link>
           </li>
+          +
           <li>
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>{totalFavorites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
